8f6cdf72a2ac4bcf1a168df522dc46c2b0cdaff8
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2023-04-21 18:40:24 +0430

    Add  class

--- public/index.php
+++ public/index.php
@@ -8,5 +8,4 @@ require_once BASE_PATH . 'vendor/autoload.php';
 require_once BASE_PATH . 'bootstrap/app.php';
 require_once BASE_PATH . 'routes.php';
 
-dd(previousUrl());
 $router = (new App\Core\Http\Router)->run();
